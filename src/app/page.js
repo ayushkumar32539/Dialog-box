c5ed449e2f24b12e79556c66abaec8b3eae3b0c6
ayushkumar32539 <ayush2010128@akgec.ac.in>
2024-04-19 22:28:27 +0530

    warnings

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,12 @@
 // pages/index.js
 
 import ReportsButton from './Components/ReportsButton';
-import ReportsDialog from './Components/ReportsDialog';
+
 
 const Home = () => {
   return (
     <div>
       <ReportsButton/>
-      {/* <ReportsDialog /> */}
     </div>
   );
 };
